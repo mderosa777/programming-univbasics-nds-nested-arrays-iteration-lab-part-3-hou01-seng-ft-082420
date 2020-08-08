4390ededa484014db619f845ee6c3f9c4c211f23
megan <meggsnbacon777@gmail.com>
2020-08-08 03:33:07 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,7 @@ def join_nested_strings(src)
   while row_index<src.count do 
     element_index = 0
     while element_index< src[row_index].count do 
-      total += src[row_index][element_index].to_s
+      total += src[row_index][element_index].join("")
     
     element_index += 1
   end
